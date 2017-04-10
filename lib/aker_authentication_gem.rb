e380e77e85efb329a723ffb2dae8a859d3537377
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-04-10 16:33:53 +0100

    Created engine to support install this gem in other services

--- lib/aker_authentication_gem.rb
+++ lib/aker_authentication_gem.rb
@@ -1,5 +1,13 @@
 require "aker_authentication_gem/version"
 
+#autoload :User, 'models/user'
+
+
+#module Users
+#  autoload :SessionsController, 'controllers/users/sessions_controller'
+#end
+
 module AkerAuthenticationGem
-  # Your code goes here...
+  class Engine < ::Rails::Engine
+  end
 end
