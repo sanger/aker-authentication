18c2cb74912ddb0d8377aac11fe9980845e99ce5
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-04-12 13:57:43 +0100

    Added require of gem dependencies

--- lib/aker_authentication_gem.rb
+++ lib/aker_authentication_gem.rb
@@ -7,7 +7,15 @@ require "aker_authentication_gem/version"
 #  autoload :SessionsController, 'controllers/users/sessions_controller'
 #end
 
+
+require "devise"
+require "net-ldap"
+require "devise_ldap_authenticatable"
+require "request_store"
+require "jwt"
+
 module AkerAuthenticationGem
   class Engine < ::Rails::Engine
+    isolate_namespace AkerAuthenticationGem
   end
 end
