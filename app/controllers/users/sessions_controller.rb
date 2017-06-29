861112461bbd3479f12026de304969302bfda646
David Robinson <dr6@sanger.ac.uk>
2017-06-29 16:33:42 +0100

    Removed commented out code in SessionsController

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,15 +1,10 @@
 module Users
   class SessionsController < Devise::SessionsController
     after_action :store_session_data, only: [:create]
-    # before_action :add_email_to_user, only: [:create]
     
     skip_authorization_check if respond_to?(:skip_authorization_check)
 
-    protected
-
-    # def add_email_to_user
-    #   request.parameters["user"]["email"] += '@sanger.ac.uk' unless request.parameters["user"]["email"].include?('@')
-    # end
+  protected
 
     def store_session_data
       session["user"] = current_user
